9ce5b6fc73f3371a963724260b5220bde635e164
Jared Hanson <jaredhanson@gmail.com>
2014-05-09 10:27:37 -0700

    Generated package structure.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('sd-zookeeper', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
